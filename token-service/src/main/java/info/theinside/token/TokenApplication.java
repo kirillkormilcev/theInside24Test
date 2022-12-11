aa8c160a1370dbba592c22747d14b557d5479f06
kirill <kirill@kormilcev.ru>
2022-12-11 17:23:34 +0300

    maven modules

---
+++ token-service/src/main/java/info/theinside/token/TokenApplication.java
@@ -0,0 +1,12 @@
+package info.theinside.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TokenApplication.class, args);
+	}
+
+}
